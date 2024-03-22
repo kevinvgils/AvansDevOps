592520bdc495dbfc7d4706c911f72540abab575a
kvnva <kvnvangils@gmail.com>
2024-03-22 15:30:45 +0100

    added git commands with command pattern

---
+++ AvansDevOps/Git/GitCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvansDevOps.Git {
+    public abstract class GitCommand {
+        public abstract void Execute();
+    }
+}
---
+++ AvansDevOps/Git/GitPush.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvansDevOps.Git {
+    public class GitPush : GitCommand {
+        public override void Execute() {
+            Console.WriteLine("git push");
+        }
+    }
+}
