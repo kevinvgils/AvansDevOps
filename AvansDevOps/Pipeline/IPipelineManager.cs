4c70974332a01ca096bbf3e44f5bda53f471fa50
kvnva <kvnvangils@gmail.com>
2024-03-22 15:53:20 +0100

    pipeline adapter

---
+++ AvansDevOps/Pipeline/IPipelineManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvansDevOps.Pipeline {
+    public interface IPipelineManager {
+        public void RetrieveSourceCode();
+        public void InstallPackages();
+        public void BuildSoftware();
+        public void ExecuteTests();
+        public void RunCodeAnalysis();
+        public void DeployToAzure();
+        public void PerformUtilityActions();
+    }
+}
---
+++ AvansDevOps/Pipeline/PipelineAdapter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvansDevOps.Pipeline {
+    public class PipelineAdapter : IPipelineManager {
+        public void BuildSoftware() {
+            Console.WriteLine("Build software");
+        }
+
+        public void DeployToAzure() {
+            Console.WriteLine("Deploy to azure");
+        }
+
+        public void ExecuteTests() {
+            Console.WriteLine("Execute Tests");
+        }
+
+        public void InstallPackages() {
+            Console.WriteLine("Install Packages");
+        }
+
+        public void PerformUtilityActions() {
+            Console.WriteLine("Perform UtilityActions");
+        }
+
+        public void RetrieveSourceCode() {
+            Console.WriteLine("Retrieve Source Code");
+        }
+
+        public void RunCodeAnalysis() {
+            Console.WriteLine("Run Code Analysis");
+        }
+    }
+}
