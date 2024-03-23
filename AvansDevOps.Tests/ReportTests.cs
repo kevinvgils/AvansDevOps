252c649dd10b39d723041138e786aaefdf9ea12a
kvnva <kvnvangils@gmail.com>
2024-03-23 22:34:47 +0100

    test

--- AvansDevOps.Tests/ReportTests.cs
+++ AvansDevOps.Tests/ReportTests.cs
@@ -113,8 +113,8 @@ namespace AvansDevOps.Tests {
             baseReport.Export(pdfStrategy, "TestReport.pdf", sprint);
             baseReport.Export(pngStrategy, "TestReport.png", sprint);
             string[] consoleOutputLines = sw.ToString().Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-            string pngLine = consoleOutputLines[0];
-            string pdfLine = consoleOutputLines[2];
+            string pngLine = consoleOutputLines[1];
+            string pdfLine = consoleOutputLines[3];
 
 
             // Assert
