380ac377d8845750e674d421b204a257f8d86fa8
kvnva <kvnvangils@gmail.com>
2024-03-24 13:58:17 +0100

    Added activity test

---
+++ AvansDevOps.Tests/ActivityTests.cs
@@ -0,0 +1,46 @@
+﻿using AvansDevOps.Backlog;
+using AvansDevOps.ScrumRole;
+using AvansDevOps.Sprint;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvansDevOps.Tests {
+    [TestFixture]
+    public class ActivityTests {
+        [Test]
+        public void TestBacklogItem_ToReadyForTestingWhenNotAllActivitysAreDone() {
+            User productowner = new(new ProductOwner());
+            Project project = new(productowner);
+            project.CreateSprint(new ReviewSprintFactory(), "ReviewSprint1");
+
+            var sprint = project.GetSprint();
+            BacklogItem item1 = new(5, "test item", sprint);
+            Activity activity = new("test activity");
+            Activity activity2 = new("test2 activity");
+            Activity activity3 = new("test3 activity");
+
+            activity.SetName("Set new Activity name");
+            item1.AddActivity(activity);
+            item1.AddActivity(activity2); 
+            item1.AddActivity(activity3);
+            activity2.SetStatus(true);
+            activity3.SetStatus(true);
+
+            item1.HandleDoing();
+            void testDelegate() => item1.HandleReadyForTesting();
+
+            Assert.Multiple(() => {
+                Assert.That(activity.GetName(), Is.EqualTo("Set new Activity name"));
+                Assert.That(activity.GetStatus(), Is.EqualTo(false));
+                Assert.That(activity2.GetStatus(), Is.EqualTo(true));
+                Assert.Throws<ArgumentException>(testDelegate);
+            });
+
+
+
+        }
+    }
+}
